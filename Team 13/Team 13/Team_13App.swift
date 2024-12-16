fa73e69b7b59011f9bd859583b8a9039bc73a510
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2024-12-16 10:30:09 +0300

    new file

---
+++ Team 13/Team 13/Team_13App.swift
@@ -0,0 +1,17 @@
+//
+//  Team_13App.swift
+//  Team 13
+//
+//  Created by atheer alshareef on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Team_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
